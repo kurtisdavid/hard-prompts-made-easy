fc07215441a916cf3ec690f2e25059c5387481fc
Kurtis David <kurtis.e.david@gmail.com>
2023-12-15 22:24:18 -0600

    add support for sd guidance

--- optim_utils.py
+++ optim_utils.py
@@ -90,13 +90,17 @@ def download_image(url):
         return None
     return Image.open(BytesIO(response.content)).convert("RGB")
 
+def encode_images(model, preprocess, device):
+    with torch.no_grad():
+        curr_images = [preprocess(target_image).unsqueeze(0) for target_image in target_images]
+        curr_images = torch.concatenate(curr_images).to(device)
+        all_target_features = model.encode_image(curr_images)
+    return all_target_features
+
 
 def get_target_feature(model, preprocess, tokenizer_funct, device, target_images=None, target_prompts=None):
     if target_images is not None:
-        with torch.no_grad():
-            curr_images = [preprocess(i).unsqueeze(0) for i in target_images]
-            curr_images = torch.concatenate(curr_images).to(device)
-            all_target_features = model.encode_image(curr_images)
+        all_target_features = encode_images(model, preprocess, device)
     else:
         texts = tokenizer_funct(target_prompts).to(device)
         all_target_features = model.encode_text(texts)
@@ -218,8 +222,147 @@ def optimize_prompt_loop(model, tokenizer, token_embedding, all_target_features,
 
     return best_text
 
+def optimize_prompt_loop_with_sd_guidance(
+    model, 
+    tokenizer, 
+    token_embedding, 
+    all_target_features, 
+    args, 
+    device,
+    preprocess,
+    sd_pipeline,
+):
+    opt_iters = args.iter
+    lr = args.lr
+    weight_decay = args.weight_decay
+    print_step = args.print_step
+    batch_size = args.batch_size
+    print_new_best = getattr(args, 'print_new_best', False)
 
-def optimize_prompt(model, preprocess, args, device, target_images=None, target_prompts=None):
+    sd_args = args.sd_args
+    alpha = sd_args["alpha"]
+    guidance_scale=sd_args["guidance_scale"]
+    num_inference_steps=sd_args["num_inference_steps"]
+    height=sd_args["height"]
+    width=sd_args["width"]
+
+
+    # initialize prompt
+    prompt_embeds, dummy_embeds, dummy_ids = initialize_prompt(tokenizer, token_embedding, args, device)
+    p_bs, p_len, p_dim = prompt_embeds.shape
+
+    # get optimizer
+    input_optimizer = torch.optim.AdamW([prompt_embeds], lr=lr, weight_decay=weight_decay)
+
+    best_loss = 1e30 * args.loss_weight
+    best_text = ""
+
+    for step in range(opt_iters):
+        # randomly sample sample images and get features
+        if batch_size is None:
+            target_features = all_target_features
+        else:
+            curr_indx = torch.randperm(len(all_target_features))
+            target_features = all_target_features[curr_indx][0:batch_size]
+            
+        universal_target_features = all_target_features
+        
+        # forward projection
+        projected_embeds, nn_indices = nn_project(prompt_embeds, token_embedding, print_hits=False)
+
+        # get cosine similarity score with all target features
+        with torch.no_grad():
+            # padded_embeds = copy.deepcopy(dummy_embeds)
+            padded_embeds = dummy_embeds.detach().clone()
+            padded_embeds[dummy_ids == -1] = projected_embeds.reshape(-1, p_dim)
+            logits_per_image, _ = model.forward_text_embedding(padded_embeds, dummy_ids, universal_target_features)
+            scores_per_prompt = logits_per_image.mean(dim=0)
+            universal_cosim_score = scores_per_prompt.max().item()
+            best_indx = scores_per_prompt.argmax().item()
+
+            decoded_text = decode_ids(nn_indices, tokenizer)[best_indx]
+
+            # Also get the new sd guidance targets...
+            # TODO: move hard coded values to config
+            sd_images = sd_pipeline(
+                decoded_text,
+                num_images_per_prompt=1,
+                guidance_scale=guidance_scale,
+                num_inference_steps=num_inference_steps,
+                height=height,
+                width=width,
+            )
+            sd_target_features = encode_images(model, preprocess, device)
+
+        
+        # tmp_embeds = copy.deepcopy(prompt_embeds)
+        tmp_embeds = prompt_embeds.detach().clone()
+        tmp_embeds.data = projected_embeds.data
+        tmp_embeds.requires_grad = True
+        
+        # padding
+        # padded_embeds = copy.deepcopy(dummy_embeds)
+        padded_embeds = dummy_embeds.detach().clone()
+        padded_embeds[dummy_ids == -1] = tmp_embeds.reshape(-1, p_dim)
+        
+        # compute training loss here
+        logits_per_image, _ = model.forward_text_embedding(padded_embeds, dummy_ids, target_features)
+        cosim_scores = logits_per_image
+        loss = 1 - cosim_scores.mean()
+        # compute sd guidance term
+        sd_guidance_logits_per_image, _ = model.forward_text_embedding(padded_embeds, dummy_ids, sd_target_features)
+        cosim_scores_sd = sd_guidance_logits_per_image
+        loss_sd = 1 - cosim_scores_sd.mean()
+        
+        # Combine loss terms for final loss.
+        loss = (1 - alpha) * loss + (alpha) * loss_sd
+        loss = loss * args.loss_weight
+
+        
+        prompt_embeds.grad, = torch.autograd.grad(loss, [tmp_embeds])
+        
+        input_optimizer.step()
+        input_optimizer.zero_grad()
+
+        curr_lr = input_optimizer.param_groups[0]["lr"]
+        cosim_scores = cosim_scores.mean().item()
+        cosim_scores_sd = cosim_scores_sd.mean().item()
+
+        if print_step is not None and (step % print_step == 0 or step == opt_iters-1):
+            per_step_message = f"step: {step}, lr: {curr_lr}"
+            if not print_new_best:
+                per_step_message = f"\n{per_step_message}, cosim: {universal_cosim_score:.3f}, cosim_sd_guidance: {cosim_scores_sd:.3f} text: {decoded_text}"
+            print(per_step_message)
+
+        if loss.item() < best_loss:
+            best_loss = loss.item()
+            best_text = decoded_text
+            if print_new_best:
+                print(f"new best loss: {best_sim}")
+                print(f"new best prompt: {best_text}")
+
+
+    if print_step is not None:
+        print()
+        print(f"best cosine sim: {best_sim}")
+        print(f"best prompt: {best_text}")
+
+    return best_text
+
+
+def optimize_prompt(
+    model, 
+    preprocess, 
+    args, 
+    device, 
+    target_images=None, 
+    target_prompts=None,
+    sd_pipeline=None,
+):
+    """Run prompt inversion training. 
+
+    If sd_pipeline is provided, enables second task for guidance with Stable Diffusion (SD).
+    """
     token_embedding = model.token_embedding
     tokenizer = open_clip.tokenizer._tokenizer
     tokenizer_funct = open_clip.get_tokenizer(args.clip_model)
@@ -228,7 +371,19 @@ def optimize_prompt(model, preprocess, args, device, target_images=None, target_
     all_target_features = get_target_feature(model, preprocess, tokenizer_funct, device, target_images=target_images, target_prompts=target_prompts)
 
     # optimize prompt
-    learned_prompt = optimize_prompt_loop(model, tokenizer, token_embedding, all_target_features, args, device)
+    if not sd_pipeline:
+        learned_prompt = optimize_prompt_loop(model, tokenizer, token_embedding, all_target_features, args, device)
+    else:
+        learned_prompt = optimize_prompt_loop_with_sd_guidance(
+            model,
+            tokenizer, 
+            token_embedding, 
+            all_target_features, 
+            args, 
+            device,
+            preprocess,
+            sd_pipeline,
+        )
 
     return learned_prompt
     
